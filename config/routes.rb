ed2c0485b0d1a8f83d7b560a0b8e6b284e123a40
ollie dwyer <olliedwyer@gmail.com>
2025-05-22 20:57:39 +1000

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post '/tasks', to: 'tasks#create'
+  patch '/tasks/:id/complete', to: 'tasks#complete', as: :complete_task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
